fe57ce677fd911a2272ef46f6d6d0db008d41ba4
Juan Manuel Mougan <juanmougan@gmail.com>
2020-05-21 22:07:54 -0300

    not working

---
+++ public/src/quepido.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+// Render everything
+console.log('before render');
+const domContainer = document.querySelector('#app-container');
+ReactDOM.render(e(LikeButton), domContainer);
+console.log('after render');
