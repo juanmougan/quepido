1790dd3a5b54ab284814c53f92a05a1017032743
Juan Manuel Mougan <juanmougan@gmail.com>
2020-05-21 23:18:32 -0300

    More tests

--- spec/tests.rb
+++ spec/tests.rb
@@ -6,19 +6,62 @@ describe "QuePido" do
     @todas = @clasicas + @etnicas
     goto "http://localhost:4567"
   end
-  
-  it "gets an ethnic food" do
+
+  it "loads and gets a classic food" do
     # GIVEN page is loaded
+    expect_base_text_to_be_present
+    # WHEN meal is loaded THEN it is a Classic meal
+    expect_chosen_text_to_be_one_of(@clasicas)
+  end
+  
+  it "gets an ethnic meal when etnicas is clicked" do
+    # GIVEN page is loaded
+    expect_base_text_to_be_present
     etnicas = browser.label id: 'etnicas'
     expect(etnicas).to be_present
-    expect(browser.text.include?("Podríamos pedir ")).to be true
 
     # WHEN etnicas is clicked
     etnicas.click
     
     # THEN one of the "etnicas" meals is returned
+    expect_chosen_text_to_be_one_of @etnicas
+    # TODO add assertion: not one of Clasicas
+  end
+
+  it "gets an classic meal when clasicas is clicked" do
+    # GIVEN page is loaded
+    expect_base_text_to_be_present
+    clasicas = browser.label id: 'clasicas'
+    expect(clasicas).to be_present
+
+    # WHEN clasicas is clicked
+    clasicas.click
+    
+    # THEN one of the "clasicas" meals is returned
+    expect_chosen_text_to_be_one_of @clasicas
+    # TODO add assertion: not one of Etnicas
+  end
+
+  it "gets any meal when todas is clicked" do
+    # GIVEN page is loaded
+    expect_base_text_to_be_present
+    todas = browser.label id: 'todas'
+    expect(todas).to be_present
+
+    # WHEN todas is clicked
+    todas.click
+    
+    # THEN one of the "todas" meals is returned
+    todas = @clasicas + @etnicas
+    expect_chosen_text_to_be_one_of todas
+  end
+
+  def expect_base_text_to_be_present
+    expect(browser.text.include?("Podríamos pedir ")).to be true
+  end
+
+  def expect_chosen_text_to_be_one_of(list)
     chosen = browser.strong class: 'item-chosen'
-    puts "chosen.text: #{chosen.text}"
-    expect(chosen.text).to any_element_from_list_includes(@etnicas)
+    expect(chosen.text).to any_element_from_list_includes(list)
   end
 end
