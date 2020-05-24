e59f79377c0783a4dcabef2d127b7fed8dac8e89
Juan Manuel Mougan <juanmougan@gmail.com>
2020-05-23 21:48:15 -0300

    Everything done, but alert now shown

--- public/ajax.js
+++ public/ajax.js
@@ -15,13 +15,21 @@ function getFromEndpoint(endpoint, textPrefix) {
 
 function acceptOrGetAnotherMeal(acceptOrReject) {
   if (acceptOrReject == 'accept-btn') {
-    // TODO show snackbar
+    showMealAccepted();
   } else {
     const alternativeTextPrefix = 'Entonces pidamos';
     let mealType = $("input[name='categorias']:checked").val();
     console.log(`mealType: ${mealType}`);
     getFromEndpoint(mealType, alternativeTextPrefix);
   }
+}
+
+function showMealAccepted() {
+  $('#accepted-meal-alert').html(
+    '<p>Genial que te haya gustado la idea!</p><button type="button" class="close">&times;</button>'
+  );
+  $('#accept-btn').prop('disabled', true);
+  $('#reject-btn').prop('disabled', true);
 }
 
 $(document).ready(function () {
@@ -33,5 +41,8 @@ $(document).ready(function () {
     let acceptOrReject = $(this).attr('id');
     console.log('Clicked categories with value: ' + acceptOrReject);
     acceptOrGetAnotherMeal(acceptOrReject);
+  });
+  $('.close').click(function () {
+    $('#accepted-meal-alert').alert('close');
   });
 });
