9d28e930bbbbbedbffedb74f63e591bb515ada83
Juan Manuel Mougan <juanmougan@gmail.com>
2020-05-23 21:02:59 -0300

    Added functionality for buttons

--- public/ajax.js
+++ public/ajax.js
@@ -1,20 +1,37 @@
-$(document).ready(function() {
-  getFromEndpoint('clasicas');
+const defaultTextPrefix = 'Podríamos pedir';
+$(document).ready(function () {
+  getFromEndpoint('clasicas', defaultTextPrefix);
 });
 
-function getFromEndpoint(endpoint) {
-  $(document).ready(function() {
-    $.get('que' + '/' + endpoint, function(data) {
+function getFromEndpoint(endpoint, textPrefix) {
+  $(document).ready(function () {
+    $.get('que' + '/' + endpoint, function (data) {
       $('#que-pido').html(
-        'Podríamos pedir <strong class="item-chosen">' + data + '</strong>'
+        `${textPrefix} <strong class="item-chosen"> ${data} </strong>`
       );
     });
   });
 }
 
-$(document).ready(function() {
-  $('.categorias').click(function() {
-    var endpoint = $(this).attr('id');
-    getFromEndpoint(endpoint);
+function acceptOrGetAnotherMeal(acceptOrReject) {
+  if (acceptOrReject == 'accept-btn') {
+    // TODO show snackbar
+  } else {
+    const alternativeTextPrefix = 'Entonces pidamos';
+    let mealType = $("input[name='categorias']:checked").val();
+    console.log(`mealType: ${mealType}`);
+    getFromEndpoint(mealType, alternativeTextPrefix);
+  }
+}
+
+$(document).ready(function () {
+  $('.categorias').click(function () {
+    let endpoint = $(this).attr('id');
+    getFromEndpoint(endpoint, defaultTextPrefix);
+  });
+  $('.accept-reject-btn').click(function () {
+    let acceptOrReject = $(this).attr('id');
+    console.log('Clicked categories with value: ' + acceptOrReject);
+    acceptOrGetAnotherMeal(acceptOrReject);
   });
 });
