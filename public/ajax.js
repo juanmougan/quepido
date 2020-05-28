7ba3f09b54e6f8fac18548330a37e0f8a5f6bb92
Juan Manuel Mougan <juanmougan@gmail.com>
2020-05-27 22:04:42 -0300

    Remove debug code, smaller buttons

--- public/ajax.js
+++ public/ajax.js
@@ -15,21 +15,17 @@ function getFromEndpoint(endpoint, textPrefix) {
 
 function acceptOrGetAnotherMeal(acceptOrReject) {
   if (acceptOrReject == 'accept-btn') {
-    console.log('Meal accepted.');
     $('#accept-btn').prop('disabled', true);
     $('#reject-btn').prop('disabled', true);
     showMealAccepted();
   } else {
-    console.log('Meal rejected!');
     const alternativeTextPrefix = 'Entonces pidamos';
     let mealType = $("input[name='categorias']:checked").val();
-    console.log(`mealType: ${mealType}`);
     getFromEndpoint(mealType, alternativeTextPrefix);
   }
 }
 
 function showMealAccepted() {
-  console.log('showing meal accepted...');
   $('#accepted-meal-alert').show();
 }
 
@@ -42,7 +38,6 @@ function loadClassicMeal() {
 
 function handleAcceptRejectClick(params) {
   $(document.body).on('click', '.accept-reject-btn', function () {
-    console.log('Clicked accept-reject button...');
     let acceptOrReject = $(this).attr('id');
     acceptOrGetAnotherMeal(acceptOrReject);
   });
